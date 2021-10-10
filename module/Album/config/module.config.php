f20bdfbb2696faa705bad0488fe231de71b18c75
Raul Bethencourt <raul.bethencourt.pro@gmail.com>
2021-10-10 23:18:14 +0200

    ✨ add Controller and module

--- module/Album/config/module.config.php
+++ module/Album/config/module.config.php
@@ -2,6 +2,7 @@
 
 namespace Album;
 
+use Laminas\Router\Http\Segment;
 use Laminas\ServiceManager\Factory\InvokableFactory;
 
 return [
@@ -9,6 +10,24 @@ return [
         'factories' => [
             Controller\AlbumController::class => InvokableFactory::class,
         ],
+    ],
+    'router' => [
+        'routes' => [
+            'album' => [
+                'type'    => Segment::class,
+                'options' => [
+                    'route' => '/album[/:action[/:id]]',
+                    'constraints' => [
+                        'action'  => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id'      => '[0-9+]',
+                    ],
+                    'defaults' => [
+                        'controller' => Controller\AlbumController::class,
+                        'action'     => 'index',
+                    ]
+                ]
+            ]
+        ]
     ],
     'view_manager' => [
         'template_path_stack' => [
