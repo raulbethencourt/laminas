1b8665b8d74f90837a467266488225ea09199974
Raul Bethencourt <raul.bethencourt.pro@gmail.com>
2021-10-16 21:12:29 +0200

    ✨ adding tests

---
+++ module/Album/test/Controller/AlbumControllerTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace AlbumTest\Controller;
+
+use Album\Controller\AlbumController;
+use Laminas\Stdlib\ArrayUtils;
+use Laminas\Test\PHPUnit\Controller\AbstractHttpControllerTestCase;
+use Album\Model\AlbumTable;
+use Laminas\ServiceManager\ServiceManager;
+
+class AlbumControllerTest extends AbstractHttpControllerTestCase
+{
+    protected $traceError = true;
+    protected $albumTable;
+
+    protected function setUp(): void
+    {
+        // The module configuration should still be applicable for tests.
+        // You can override configuration here with test case specific values,
+        // such as sample view templates, path stacks, module_listener_options,
+        // etc.
+        $configOverrides = [];
+
+        $this->setApplicationConfig(ArrayUtils::merge(
+            // Grabbing the full application configuration:
+            include __DIR__ . '/../../../../config/application.config.php',
+            $configOverrides
+        ));
+        parent::setUp();
+
+        $this->configureServiceManager($this->getApplicationServiceLocator());
+    }
+
+    public function testIndexActonCanBeAccessed()
+    {
+        $this->albumTable->fetchAll()->willReturn([]);
+
+        $this->dispatch('/album');
+        $this->assertResponseStatusCode(200);
+        $this->assertModuleName('Album');
+        $this->assertControllerName(AlbumController::class);
+        $this->assertControllerClass('AlbumController');
+        $this->assertMatchedRouteName('album');
+    }
+
+    protected function configureServiceManager(ServiceManager $services)
+    {
+        $services->setAllowOverride(true);
+
+        $services->setService('config', $this->updateConfig($services->get('config')));
+        $services->setService(AlbumTable::class, $this->mockAlbumTable()->reveal());
+
+        $services->setAllowOverride(false);
+    }
+
+    protected function updateConfig($config)
+    {
+        $config['db'] = [];
+        return $config;
+    }
+
+    protected function mockAlbumTable()
+    {
+        $this->albumTable = $this->prophesize(AlbumTable::class);
+        return $this->albumTable;
+    }
+}
